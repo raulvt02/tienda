68a71db8edf5a8d138c98312e9dc8600a3b693c0
raulvt02 <vera.trashorras.raul@iescamas.es>
2025-03-19 21:21:47 +0100

    commit

--- Tienda/src/test/java/com/miempresa/entidad/ProductoTest.java
+++ Tienda/src/test/java/com/miempresa/entidad/ProductoTest.java
@@ -7,14 +7,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class ProductoTest {
+@DisplayName("Realización de la prueba unitaria de la entidad producto")
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+class ProductoTest {
+	
+	Producto producto;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		producto = new Producto("Portátil", 500);
 	}
 
 	@Test
@@ -23,7 +24,28 @@ class ProductoTest {
 		String nombreEsperado = "Portátil";
 		double precioEsperado = 500;
 		
-		assertEquals(nombreEsperado, Producto.getNombre());
+		assertEquals(nombreEsperado, producto.getNombre());
+		assertEquals(precioEsperado, producto.getPrecio());
 	}
+	
+	@Test
+	@DisplayName("Debería lanzar una excepción cuando al crear un producto el precio sea negativo")
+	void testCrearUnProductoInvalidoPrecio() {
+		Exception exception = assertThrows(IllegalArgumentException.class, ()->{new Producto("PC", -3);});
+		double precioEsperado = -3.0;
+		String mensajeEsperado = "El precio no puede ser negativo: " + precioEsperado;
+		
+		assertEquals(mensajeEsperado, producto.getPrecio());
+	}
+	
+	@Test
+	@DisplayName("Deberia modificar el nombre del producto")
+	void testModificarNombreProducto() {
+		String nombreEsperado = "Monitor";
+		producto.setNombre("Monitor");
+		
+		assertEquals(nombreEsperado, producto.getNombre(), "El nombre no se ha modificado correctamente desde el método set");
+	}
+	
 
 }
